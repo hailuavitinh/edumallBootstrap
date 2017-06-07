2bf589f0bbf6586b1711a7459f47d88b3eeed5e4
thanhdc <dang.phongvan.thanh@gmail.com>
2017-06-08 06:32:30 +0700

    initial web 7

---
+++ web7/js/web.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    var docao = $(window).height();
+    console.log(docao);
+    $('.top1').css({ 'height': docao });
+
+    $(window).resize(function () {
+        var docao = $(window).height();
+        $('.top1').css({ 'height': docao });
+    });
+
+});

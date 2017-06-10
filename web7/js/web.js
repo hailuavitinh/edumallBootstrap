06324b2a94b579674983e926fbecdac16bb1dca5
thanhdc <dang.phongvan.thanh@gmail.com>
2017-06-10 07:10:35 +0700

    gsdgsdfasf

--- web7/js/web.js
+++ web7/js/web.js
@@ -4,9 +4,9 @@ $(function () {
     console.log(docao);
     $('.top1').css({ 'height': docao });
 
-    // $(window).resize(function () {
-    //     var docao = $(window).height();
-    //     $('.top1').css({ 'height': docao });
-    // });
+    $(window).resize(function () {
+        var docao = $(window).height();
+        $('.top1').css({ 'height': docao });
+    });
 
 });

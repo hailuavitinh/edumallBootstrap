7edd226979cbaa284cfa694b425c843db456040c
thanhdc <dang.phongvan.thanh@gmail.com>
2017-06-13 06:11:29 +0700

    Done effect web 8

--- web8/js/web.js
+++ web8/js/web.js
@@ -1,5 +1,21 @@
 $(function () {
 
+    $(window).scroll(function(){
+        var vitri = $('body').scrollTop();
+        if(vitri > 50){
+            $('.navbar-fixed-top').addClass('menuchay');
+        } else {
+            $('.navbar-fixed-top').removeClass('menuchay');
+        }
+    });
     
+    var speedscroll = 1800;
+
+    $('.navbar-right li a').click(function(){
+        var position = $(this).attr('href');
+        $('body').animate({scrollTop:$(position).offset().top},speedscroll,"easeInOutBack");
+        $(this).blur();
+        return false;
+    });
 
 });
